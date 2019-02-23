15349a5ec5b406f63a0ac50d6fbd8fd38ced6d00
Your Name <you@example.com>
2019-02-23 10:37:51 -0300

    configuring mongodb as database

--- src/server.js
+++ src/server.js
@@ -1,21 +1,30 @@
-const express = require("express");
-const path = require("path");
+const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
 
 class App {
-  constructor() {
-    this.express = express();
-    this.isDev = process.env.NODE_ENV != "production";
+  constructor () {
+    this.express = express()
+    this.isDev = process.env.NODE_ENV != 'production'
 
-    this.middlewares();
-    this.routes();
+    this.middlewares()
+    this.database()
+    this.routes()
   }
 
-  middlewares() {
-    this.express.use(express.json());
+  database () {
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
   }
 
-  routes() {
-    this.express.use(require("./routes"))
+  middlewares () {
+    this.express.use(express.json())
+  }
+
+  routes () {
+    this.express.use(require('./routes'))
   }
 }
 
